09a0c45112286f20a94d8c56d47ead5efd997e50
BuyMyMojo <hello@buymymojo.net>
2023-02-17 21:31:08 +1100

    cargo fmt

--- src/lib/utils.rs
+++ src/lib/utils.rs
@@ -1,7 +1,11 @@
 pub fn return_truncated(string: String, max_length: usize) -> String {
     if string.len() > max_length {
-        string.chars().take(max_length - 5).chain("[...]".chars()).collect()
+        string
+            .chars()
+            .take(max_length - 5)
+            .chain("[...]".chars())
+            .collect()
     } else {
-    string
+        string
     }
 }
