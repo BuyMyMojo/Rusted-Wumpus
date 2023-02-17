d767fede425fd85e0b78bdd5d3742d4717d51feb
BuyMyMojo <hello@buymymojo.net>
2023-02-17 21:30:54 +1100

    Account for unicode

--- src/lib/utils.rs
+++ src/lib/utils.rs
@@ -1,7 +1,7 @@
-pub fn return_truncated(mut string: String, max_length: usize) -> String {
+pub fn return_truncated(string: String, max_length: usize) -> String {
     if string.len() > max_length {
-        string.truncate(max_length - 5);
-        string.push_str("[...]");
-    }
+        string.chars().take(max_length - 5).chain("[...]".chars()).collect()
+    } else {
     string
+    }
 }
